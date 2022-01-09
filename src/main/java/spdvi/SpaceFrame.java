bdde1ce8f44d71dda8438f2571dbc0a4e4fbab19
AdrianGonzalezB <adriangonzalez@paucasesnovescifp.cat>
2022-01-09 12:42:49 +0100

    DownloadImage movido a dataAccess
    - Se ha movido el método dowloadImage a la clase dataAccess para poder acceder a el cuando se quiera  mediante cuatro parámetros.

--- src/main/java/spdvi/SpaceFrame.java
+++ src/main/java/spdvi/SpaceFrame.java
@@ -541,65 +541,16 @@ public class SpaceFrame extends javax.swing.JFrame implements Runnable {
 //            System.out.println(Thread.currentThread().getName());
             downloadThread = new Thread(this);
             downloadThread.start();
-            downloadImage();
+            da.downloadImage(containerClient,prgDownloadImage,cboImagen,lblImage);
                    
         }
        
     }//GEN-LAST:event_cboImagenActionPerformed
     
-    
-    private void downloadImage() {
-        // Downloading big images in chunks of 1kB might be very slow because of the request overhead to azure. Modify the algorithm to donwload eavery image in, for instance 20 chunks.
-
-        ByteArrayOutputStream outputStream;
-        BufferedImage originalImage;
-        try {
-            BlockBlobClient blobClient = containerClient.getBlobClient(cboImagen.getSelectedItem().toString()).getBlockBlobClient();
-            int dataSize = (int) blobClient.getProperties().getBlobSize();
-//            int numberOfBlocks = dataSize / 1024;
-            int numberOfBlocks = 20;
-            int numberOfBPerBlock = dataSize / numberOfBlocks;  // Split every image in 20 blocks. That is, make 20 requests to Azure.
-            System.out.println("Starting download of " + dataSize + " bytes in " + numberOfBlocks + " " + numberOfBPerBlock/1024 + "kB chunks");
-
-            
-            int i = 0;
-            outputStream = new ByteArrayOutputStream(dataSize);
-
-            while (i < numberOfBlocks) {
-                BlobRange range = new BlobRange(i * numberOfBPerBlock, (long)numberOfBPerBlock);
-                DownloadRetryOptions options = new DownloadRetryOptions().setMaxRetryRequests(5);
-
-                System.out.println(i + ": Downloading bytes " + range.getOffset() + " to " + (range.getOffset() + range.getCount()) + " with status "
-                        + blobClient.downloadStreamWithResponse(outputStream, range, options, null, false,
-                                Duration.ofSeconds(30), Context.NONE));
-                i++;
-                prgDownloadImage.setValue(i * prgDownloadImage.getMaximum() / (numberOfBlocks + 1));
-            }
-
-            // Download the last bytes of the image
-            BlobRange range = new BlobRange(i * numberOfBPerBlock);
-            DownloadRetryOptions options = new DownloadRetryOptions().setMaxRetryRequests(5);
-            System.out.println(i + ": Downloading bytes " + range.getOffset() + " to " + dataSize + " with status "
-                    + blobClient.downloadStreamWithResponse(outputStream, range, options, null, false,
-                            Duration.ofSeconds(30), Context.NONE));
-            i++;
-            prgDownloadImage.setValue(i * prgDownloadImage.getMaximum() / (numberOfBlocks + 1));
-            
-//            blobClient.downloadStream(outputStream);  // Thread Blocking
-            DataAccess dataAccess = new DataAccess();
-            originalImage = ImageIO.read(new ByteArrayInputStream(outputStream.toByteArray()));
-            ImageIcon icon = dataAccess.resizeImageIcon(originalImage, lblImage.getWidth(), lblImage.getHeight());
-            lblImage.setIcon(icon);
-            outputStream.close();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-    }
-    
     @Override
     public void run() {
         System.out.println(Thread.currentThread().getName());
-        downloadImage();
+        da.downloadImage(containerClient,prgDownloadImage,cboImagen,lblImage);
     }
     
    public void UpdateSpaceListView() {
@@ -628,7 +579,7 @@ public class SpaceFrame extends javax.swing.JFrame implements Runnable {
             downloadThread = new Thread(this);
             downloadThread.start();
             lblImage.setText("");
-            //lblImage.setIcon(new ImageIcon(getClass().getResource("/gif/spinner.gif"))); //ESTA COMENTADA POR QUE ME DA ERROR
+            lblImage.setIcon(new ImageIcon(getClass().getResource("/gif/spinner.gif"))); //ESTA COMENTADA POR QUE ME DA ERROR
         }
         if (selectedSpace != null) {
             String[] infoSpace = (selectedSpace.split(", "));
